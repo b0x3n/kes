3157449e10b2571af3de25ec60ee5a3296ce3e74
b0x3n <b0x3n4dm1n@gmail.com>
2024-09-19 11:46:02 +0100

    Added 'About' page

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -4,8 +4,8 @@
 
     const Main = () => {
 
-        window.__url = '';
-//        window.__url = 'https://b0x3n.github.io/kes';
+        // window.__url = '';
+        window.__url = 'https://b0x3n.github.io/kes';
 
         const   __app = App();
 
