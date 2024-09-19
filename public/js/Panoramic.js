b8390ddff309246d40552d4ef287cde872484bc5
b0x3n <b0x3n4dm1n@gmail.com>
2024-09-19 08:52:21 +0100

    Minor issue with panoramic images

--- public/js/Panoramic.js
+++ public/js/Panoramic.js
@@ -9,7 +9,7 @@
 
         objConfig = {
             'duration': 500000,
-            'url': '',
+            'url': 'https://b0x3n.github.io/kes',
             'path': '/public/images/panoramic/',
             'images': [
                 '360_landscape_1_clipped.jpg',
